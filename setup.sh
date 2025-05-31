4f1231f25fc931b6564523664f225e329d10de26
SSLRI <40920744+SSLRI@users.noreply.github.com>
2025-06-01 01:26:51 +0330

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "[*] Updating system..."
-apt update && apt upgrade -y
-echo "[*] Installing dependencies..."
-apt install -y python3 python3-pip git tor rust unzip
-pip install -r requirements.txt --no-cache-dir
-echo "[*] Done. Run with: python3 main.py"
